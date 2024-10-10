5ebb683859297ce961d1ac3be813532123a9a290
themagicdev3351 <themagicdev3351@gmail.com>
2024-10-10 10:01:39 +0530

    fixed route

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
-const PrivateRoute = ({isAuthenticated}) => { 
+const PrivateRoute = ({ isAuthenticated }) => {
     return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
-};
-
-
-
+};  
 export default PrivateRoute;
